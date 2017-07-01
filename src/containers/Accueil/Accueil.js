f95b11b755e224a84f58f0f988902ddf48d289a7
Lucas CHARVOLIN <lcharvol@e3r4p19.42.fr>
2017-07-01 18:40:06 +0200

    update

--- src/containers/Accueil/Accueil.js
+++ src/containers/Accueil/Accueil.js
@@ -4,7 +4,12 @@ import './Accueil.css';
 import TweetElem from '../../components/TweetElem';
 
 const Acceuil = () => (
-  <div className="acceuil">
+  <div className="Accueil">
+    <TweetElem />
+    <TweetElem />
+    <TweetElem />
+    <TweetElem />
+    <TweetElem />
     <TweetElem />
     <TweetElem />
     <TweetElem />
