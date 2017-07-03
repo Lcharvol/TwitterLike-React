771a4f578cc2ed0751ba693d4c3fdc3919295d76
Lucas CHARVOLIN <lcharvol@e3r8p11.42.fr>
2017-07-03 15:22:43 +0200

    update

--- src/containers/Page404/Page404.js
+++ src/containers/Page404/Page404.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import './Page404.css'
+
 const Page404 = () => (
-  <h1>ERROR 404 PAGE NOT FOUND</h1>
+  <div className="main404">
+    <h1>ERROR 404</h1>
+    <h1>PAGE NOT FOUND</h1>
+  </div>
 );
 
 export default Page404;
